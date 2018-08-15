992df92557053d64f344ca1443a2d4417e5a8d21
penssake <penssake@gmail.com>
2018-08-15 14:53:43 -0700

    link to home icon placeholder added to the header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,6 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div>
+        <a href='/'><button>TO HOME ICON</button></a>
         <h1>Four Winds Group Inc.</h1>
       </div>
 
