981f6a1bdd3e5eef8161ababf84db73de89c77cb
Jermaine Walker <jermainemwalker@gmail.com>
2018-08-15 12:26:42 -0700

    Whoops, missed one.

--- src/components/m-pet-net/index.js
+++ src/components/m-pet-net/index.js
@@ -10,7 +10,7 @@ class MpetNet extends Component {
           <li>Assets — Create, modify, and track any and all of your business's assets.</li>
           <li>Personnel — Track all of your personnel through one easy and concise screen.</li>
           <li>Preventative Maintenance — One of the biggest areas of focus of any maintenance division. A well orchestrated and followed preventative maintenance plan is invaluable down the road, saving work hours, materials, and money.</li>
-          <li>Jobs - Handle anything related to a particular Job all on one, easy to follow screen. Track and update Jobs real-time, as work is completed on them.</li>
+          <li>Jobs — Handle anything related to a particular Job all on one, easy to follow screen. Track and update Jobs real-time, as work is completed on them.</li>
           <li>Materials / Parts — Stay on top of needed spares or material to plan and complete jobs.</li>
           <li>Production — Keep track of everything from efficiency and downtime to repair time and breakdown response time quickly and easily.</li>
           <li>Reports — Print reports directly from M-PET.</li>
