c0f02d151fd361e1a7edda35e522eb925fbe6d2d
Jermaine Walker <jermainemwalker@gmail.com>
2018-08-18 00:04:14 -0700

    Nested navbar builds and displays.

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import './index.scss'
 
 let initialArray = [
-  ['Products','M-PET.net','M-PET.web'],
+  ['Products','M-PET.NET','M-PET.WEB'],
   ['Services','M-PET Hosted','Custom Programming'],
   ['Turnkey','About', 'Project Management', 'IT Tasks', 'Maintenance'],
-  ['KeyIndustries','Transportation', 'Hospitality'],
+  ['Key Industries','Transportation', 'Hospitality'],
   ['Company','Team', 'History'],
-].map((item, i) => <ul key={i}>{item}</ul>)
+]
 
 class Nav extends Component {
   constructor(props) {
@@ -17,8 +17,22 @@ class Nav extends Component {
     }
   }
 
-  render() {
+  componentWillMount() {
+    let newStateArray = []
+    for (let i = 0; i < initialArray.length; i++ ) {
+      for ( let j = 0; j < initialArray[ i ].length; j++ ) {
+        let newState = initialArray[ i ][ j ]
+        if ( j === 0 ) {
+          newStateArray.push( <ul>{newState}</ul> )
+        } else {
+          newStateArray.push( <li>{newState}</li> )
+        }
+      }
+    }
+    this.setState( { initialState: newStateArray } )
+  }
 
+  render() {
     return (
       <nav>
         {this.state.initialState}
