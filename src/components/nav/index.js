1936c40c5176a4a7675b1beec8cd6359827e4472
penssake <penssake@gmail.com>
2018-08-17 20:18:19 -0700

    fffff ok

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,48 +1,27 @@
 import React, { Component } from 'react'
 import './index.scss'
 
-class Nav extends Component {
+let initialArray = [
+  ['Products','M-PET.net','M-PET.web'],
+  ['Services','M-PET Hosted','Custom Programming'],
+  ['Turnkey','About', 'Project Management', 'IT Tasks', 'Maintenance'],
+  ['KeyIndustries','Transportation', 'Hospitality'],
+  ['Company','Team', 'History'],
+].map((item, i) => <ul key={i}>{item}</ul>)
 
-  clickTracker = (e) => {
-    e.preventDefault()
-    let activeClass = document.querySelector('.active')
-    if(activeClass)
-      activeClass.classList.remove('active')
-    e.target.classList.toggle('active')
+class Nav extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initialState: initialArray,
+    }
   }
 
   render() {
 
     return (
       <nav>
-        <li className='' onClick={this.clickTracker}>Services</li>
-        <ul>
-          <a href='/services'><li>M-PET Hosted</li></a>
-          <a href='/services'><li>Custom Programming</li></a>
-        </ul>
-        <li className='' onClick={this.clickTracker}>Turnkey</li>
-        <ul>
-          <a href='/services/turnkey'><li>About</li></a>
-          <a href='/services/turnkey'><li>Project Management</li></a>
-          <a href='/services/turnkey'><li>IT Tasks</li></a>
-          <a href='/services/turnkey'><li>Maintenance</li></a>
-        </ul>
-        <li className='' onClick={this.clickTracker}>Products</li>
-        <ul>
-          <a href='/products'><li>M-PET.NET</li></a>
-          <a href='/products'><li>M-PET.WEB</li></a>
-        </ul>
-        <li className='' onClick={this.clickTracker}>Key Industries</li>
-        <ul>
-          <a href='/key-industries'><li>Transportation</li></a>
-          <a href='/key-industries'><li>Hospitality</li></a>
-        </ul>
-        <li className='' onClick={this.clickTracker}>Company</li>
-        <ul>
-          <a href='/company'><li>Team</li></a>
-          <a href='/company'><li>History</li></a>
-          <a href='/company'><li>Partners</li></a>
-        </ul>
+        {this.state.initialState}
       </nav>
     )
   }
