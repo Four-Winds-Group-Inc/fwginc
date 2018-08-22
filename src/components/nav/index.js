d018db2256373ea6b5b793d74a1f9c689a279277
Jermaine Walker <jermainemwalker@gmail.com>
2018-08-21 22:43:42 -0400

    Not sure, but maybe most recent navbar?

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,30 +2,47 @@ import React, { Component } from 'react'
 import './index.scss'
 
 let initialArray = [
-  ['Products','M-PET.NET','M-PET.WEB'],
-  ['Services','M-PET Hosted','Custom Programming'],
-  ['Turnkey','About', 'Project Management', 'IT Tasks', 'Maintenance'],
-  ['Key Industries','Transportation', 'Hospitality'],
-  ['Company','Team', 'History'],
+  [ 'Products', 'M-PET.NET', 'M-PET.WEB' ],
+  [ 'Services', 'M-PET Hosted', 'Custom Programming' ],
+  [ 'Turnkey', 'About', 'Project Management', 'IT Tasks', 'Maintenance' ],
+  [ 'Key Industries', 'Transportation', 'Hospitality' ],
+  [ 'Company', 'Team', 'History' ],
 ]
 
 class Nav extends Component {
-  constructor(props) {
-    super(props)
+  constructor( props ) {
+    super( props )
     this.state = {
       initialState: initialArray,
+      active: false,
+    }
+  }
+
+  clickHandler = ( e ) => {
+    e.preventDefault()
+    if ( this.state.active ) {
+      this.setState( { active: false } )
+      e.target.className = this.state.active
+    } else {
+      let activeClass = document.querySelector( '.true' )
+      if ( activeClass )
+        activeClass.classList.remove( 'true' )
+      this.setState( { active: true } )
+      e.target.className = this.state.active
     }
   }
 
   componentWillMount() {
     let newStateArray = []
-    for (let i = 0; i < initialArray.length; i++ ) {
+    for ( let i = 0; i < initialArray.length; i++ ) {
       for ( let j = 0; j < initialArray[ i ].length; j++ ) {
-        let newState = initialArray[ i ][ j ]
+        let newStateLi = initialArray[ i ][ j ]
+        let newStateUl = initialArray[ i ]
+
         if ( j === 0 ) {
-          newStateArray.push( <ul>{newState}</ul> )
+          newStateArray.push( <li onClick={this.clickHandler}>{newStateLi}</li> )
         } else {
-          newStateArray.push( <li>{newState}</li> )
+          newStateArray.push( <ul>{newStateUl}</ul> )
         }
       }
     }
