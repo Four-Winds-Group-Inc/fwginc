ba256d7612a11699a3afde31cebe5e767b70f48e
penssake <penssake@gmail.com>
2018-09-07 15:12:54 -0700

    staff card update

--- src/components/company/staff/index.js
+++ src/components/company/staff/index.js
@@ -7,23 +7,33 @@ class Staff extends Component {
         <div className='staff-card-container'>
           <div className='staff-card-top'><h1>KEY STAFF</h1></div>
           <div className='staff-card-content'>
-            <h3>Name Here</h3>
-            <h4>Title Here</h4>
-            <p>Email | 000.000.0000</p></div>
+            <h3>TOM THOMPSON</h3>
+            <h4>NW Region</h4>
+            <p>thompsont@fwginc.com</p>
+            <p>619.921.5846</p></div>
           <div className='staff-card-content'>
-            <h3>Name Here</h3>
-            <h4>Title Here</h4>
-            <p>Email | 000.000.0000</p>
+            <h3>JON MANGERICH</h3>
+            <h4>Corporate</h4>
+            <p>mangerichj@fwginc.com</p>
+            <p>858.483.7654</p>
           </div>
           <div className='staff-card-content'>
-            <h3>Name Here</h3>
-            <h4>Title Here</h4>
-            <p>Email | 000.000.0000</p>
+            <h3>TOM HAMM</h3>
+            <h4>Corporate</h4>
+            <p>hammt@fwginc.com</p>
+            <p>619.921-5845</p>
           </div>
           <div className='staff-card-content'>
-            <h3>Name Here</h3>
-            <h4>Title Here</h4>
-            <p>Email | 000.000.0000</p>
+            <h3>GREGORY DUKES</h3>
+            <h4>Developer</h4>
+            <p>dukesg@fwginc.com</p>
+            <p>206.618.2432</p>
+          </div>
+          <div className='staff-card-content'>
+            <h3>RON DUNPHY</h3>
+            <h4>Developer</h4>
+            <p>dunphyr@fwginc.com</p>
+            <p>206.515.3457</p>
           </div>
         </div>
       </div>
