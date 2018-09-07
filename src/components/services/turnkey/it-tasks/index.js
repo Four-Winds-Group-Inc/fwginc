6967f2a7a7e2d2097703144d29eb89ef41728f65
penssake <penssake@gmail.com>
2018-09-06 17:24:01 -0700

    turnkey mobile initial scss

--- src/components/services/turnkey/it-tasks/index.js
+++ src/components/services/turnkey/it-tasks/index.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 
 class ItTasks extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      open: false,
+    }
+  }
+  clickHandler = () => {
+    this.setState( { open: !this.state.open } )
+  }
   render() {
+    let toggleClass = this.state.open ? 'open' : ''
     return (
-      <div className='div-container'>
-        <h1>IT Tasks</h1>
-        <ul>
-          <li>Evaluate and recommend network, workstation and server requirements</li>
-          <li>Purchase and install any additional hardware and software</li>
-          <li>Configure existing workstations for the database and web server</li>
-          <li>Create custom reports and provide data analysis</li>
-        </ul>
+      <div className='JSX-WRAP'>
+        <div className={'initial card' + toggleClass}>
+          <div className='card-container'>
+            <div className='turnkey-card-top'>
+              <h1>IT Tasks</h1></div>
+          </div>
+          <button onClick={this.clickHandler}> &#8594; </button>
+        </div>
+        <div className={'card card-content' + toggleClass}>
+          <div className='back-card-top'><h2>FEATURES</h2></div>
+          <ul>
+            <li>Evaluate and recommend network, workstation and server requirements.</li>
+            <li>Purchase and install any additional hardware and software.</li>
+            <li>Configure existing workstations for the database and web server.</li>
+            <li>Create custom reports and provide data analysis.</li>
+          </ul>
+          <button onClick={this.clickHandler}> &#8592; </button>
+        </div>
       </div>
     )
   }
--- src/components/services/turnkey/maintenance/index.js
+++ src/components/services/turnkey/maintenance/index.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
 
 class Maintenance extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      open: false,
+    }
+  }
+  clickHandler = () => {
+    this.setState( { open: !this.state.open } )
+  }
   render() {
+    let toggleClass = this.state.open ? 'open' : ''
     return (
-      <div className='div-container'>
-        <h1>Maintenance Information Tasks</h1>
-        <ul>
-          <li>Identify all assets that will become part of the asset database</li>
-          <li>Develop asset lists with nameplate data information</li>
-          <li>Import asset data into the M-PET.NET database</li>
-          <li>Develop preventive maintenance procedures for all maintainable assets</li>
-          <li>Develop preventive maintenance schedules and link assets to tasks with frequencies</li>
-          <li>Level preventive maintenance schedules to ensure work is evenly distributed</li>
-          <li>Train users at all levels of interaction of M-PET.NET</li>
-          <li>Provide on-going support</li>
-        </ul>
+      <div className='JSX-WRAP'>
+        <div className={'initial card' + toggleClass}>
+          <div className='card-container'>
+            <div className='turnkey-card-top'>
+              <h1>Maintenance Information Tasks</h1></div>
+          </div>
+          <button onClick={this.clickHandler}> &#8594; </button>
+        </div>
+        <div className={'card card-content' + toggleClass}>
+          <div className='back-card-top'><h2>FEATURES</h2></div>
+          <ul>
+            <li>Identify all assets that will become part of the asset database.</li>
+            <li>Develop asset lists with nameplate data information.</li>
+            <li>Import asset data into the M-PET.NET database.</li>
+            <li>Develop preventive maintenance procedures for all maintainable assets.</li>
+            <li>Develop preventive maintenance schedules and link assets to tasks with frequencies.</li>
+            <li>Level preventive maintenance schedules to ensure work is evenly distributed.</li>
+            <li>Train users at all levels of interaction of M-PET.NET.</li>
+            <li>Provide on-going support.</li>
+          </ul>
+          <button onClick={this.clickHandler}> &#8592; </button>
+        </div>
       </div>
     )
   }
