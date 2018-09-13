fede36bc19205d7586e531386df275b5be3783ec
penssake <penssake@gmail.com>
2018-09-12 20:21:46 -0700

    tablet-scss initial products

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -3,11 +3,23 @@ import MpetNet from './m-pet-net'
 import MpetWeb from './m-pet-web'
 
 class Products extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      toggle: false,
+    }
+  }
+  clickHandler = () => {
+    this.setState( { toggle: !this.state.toggle } )
+  }
   render() {
+    let toggleClass = this.state.toggle ? 'toggle' : ''
+    console.log(toggleClass)
     return (
       <div className='card-container'>
-        <div><MpetNet /></div>
-        <div><MpetWeb /></div>
+        <div className={'product-one' + toggleClass}><MpetNet /></div>
+        <button button onClick={this.clickHandler} className='arrow-down'> &#8595; </button>
+        <div className={'product-two' + toggleClass}><MpetWeb /></div>
       </div>
 
     )
